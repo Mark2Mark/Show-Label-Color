b404afc760a0b8d3345c39399f4104c014f25756
schriftgestalt <georg.seifert@mac.com>
2017-07-18 23:04:19 +0800

    cleanup and new API

--- LabelColor.glyphsReporter/Contents/Resources/plugin.py
+++ LabelColor.glyphsReporter/Contents/Resources/plugin.py
@@ -8,19 +8,13 @@
 # TODO:
 #	add clipping path https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSBezierPath_Class/#//apple_ref/doc/uid/20000339-SW27
 
+from GlyphsApp.plugins import *
 import objc
 # from Foundation import *
 from AppKit import *
 import sys, os, re
 import math
 import traceback
-
-MainBundle = NSBundle.mainBundle()
-path = MainBundle.bundlePath() + "/Contents/Scripts"
-if not path in sys.path:
-	sys.path.append( path )
-
-import GlyphsApp
 
 #### OTIONS ++++++++++++++++++++++++++++++++++++++++++
 #### #######++++++++++++++++++++++++++++++++++++++++++
@@ -48,90 +42,9 @@ labelColorsDict = {
 	9223372036854775807 : (1, 1, 1, 0), # not colored, white
 }
 
-
-GlyphsReporterProtocol = objc.protocolNamed( "GlyphsReporter" )
-
-class LabelColor ( NSObject, GlyphsReporterProtocol ):
-	def init( self ):
-		"""
-		Put any initializations you want to make here.
-		"""
-		try:
-			#Bundle = NSBundle.bundleForClass_( NSClassFromString( self.className() ));
-			return self
-		except Exception as e:
-			self.logToConsole( "init: %s" % str(e) )
-		
-	def interfaceVersion( self ):
-		"""
-		Distinguishes the API version the plugin was built for. 
-		Return 1.
-		"""
-		try:
-			return 1
-		except Exception as e:
-			self.logToConsole( "interfaceVersion: %s" % str(e) )
-	
-	def title( self ):
-		"""
-		This is the name as it appears in the menu in combination with 'Show'.
-		E.g. 'return "Nodes"' will make the menu item read "Show Nodes".
-		"""
-		try:
-			return "Label Color"
-		except Exception as e:
-			self.logToConsole( "title: %s" % str(e) )
-	
-	def keyEquivalent( self ):
-		"""
-		The key for the keyboard shortcut. Set modifier keys in modifierMask() further below.
-		Pretty tricky to find a shortcut that is not taken yet, so be careful.
-		If you are not sure, use 'return None'. Users can set their own shortcuts in System Prefs.
-		"""
-		try:
-			return None
-		except Exception as e:
-			self.logToConsole( "keyEquivalent: %s" % str(e) )
-	
-	def modifierMask( self ):
-		"""
-		Use any combination of these to determine the modifier keys for your default shortcut:
-			return NSShiftKeyMask | NSControlKeyMask | NSCommandKeyMask | NSAlternateKeyMask
-		Or:
-			return 0
-		... if you do not want to set a shortcut.
-		"""
-		try:
-			return 0
-		except Exception as e:
-			self.logToConsole( "modifierMask: %s" % str(e) )
-	
-	def drawForegroundForLayer_( self, Layer ):
-		"""
-		Whatever you draw here will be displayed IN FRONT OF the paths.
-		Setting a color:
-			NSColor.colorWithCalibratedRed_green_blue_alpha_( 1.0, 1.0, 1.0, 1.0 ).set() # sets RGBA values between 0.0 and 1.0
-			NSColor.redColor().set() # predefined colors: blackColor, blueColor, brownColor, clearColor, cyanColor, darkGrayColor, grayColor, greenColor, lightGrayColor, magentaColor, orangeColor, purpleColor, redColor, whiteColor, yellowColor
-		Drawing a path:
-			myPath = NSBezierPath.alloc().init()  # initialize a path object myPath
-			myPath.appendBezierPath_( subpath )   # add subpath to myPath
-			myPath.fill()   # fill myPath with the current NSColor
-			myPath.stroke() # stroke myPath with the current NSColor
-		To get an NSBezierPath from a GSPath, use the bezierPath() method:
-			myPath.bezierPath().fill()
-		You can apply that to a full layer at once:
-			if len( myLayer.paths > 0 ):
-				myLayer.bezierPath()       # all closed paths
-				myLayer.openBezierPath()   # all open paths
-		See:
-		https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSBezierPath_Class/Reference/Reference.html
-		https://developer.apple.com/library/mac/documentation/cocoa/reference/applicationkit/classes/NSColor_Class/Reference/Reference.html
-		"""
-		try:
-			pass
-		except Exception as e:
-			self.logToConsole( "drawForegroundForLayer_: %s" % str(e) )
-	
+class LabelColor (ReporterPlugin):
+	def settings(self):
+		self.menuName = Glyphs.localize({'en': u'Label Color', 'de': u'Label Farbe'})
 
 	def BlockOutGlyph( self, Layer ):
 		if drawingOption == "Label Size":
@@ -160,7 +73,6 @@ class LabelColor ( NSObject, GlyphsReporterProtocol ):
 			except:
 				# Glyphs 1.x or no layerColor:
 				thisColor = (1, 1, 1, 0)
-
 			try:
 				layerColor = Layer.colorObject # Layer.color()
 			except:
@@ -182,7 +94,6 @@ class LabelColor ( NSObject, GlyphsReporterProtocol ):
 					thisGlyphColor = 1, 1, 1, 0
 				NSColor.colorWithCalibratedRed_green_blue_alpha_( *thisGlyphColor ).set()
 
-				
 				if drawingOption == "Label Size":
 					rectangle = [0, 0, thisWidth, -40]
 				elif drawingOption == "Label Size Descender":
@@ -193,9 +104,7 @@ class LabelColor ( NSObject, GlyphsReporterProtocol ):
 					ySize = upm+thisDescender
 					rectangle = [0, thisDescender, thisWidth, ySize]
 				
-
-
-				if layerColor != None:				
+				if layerColor != None:
 					'''
 					LEFT = Glyph-Color
 					'''
@@ -223,8 +132,6 @@ class LabelColor ( NSObject, GlyphsReporterProtocol ):
 					NSColor.colorWithCalibratedRed_green_blue_alpha_( *thisLayerColor ).set()
 
 					pathRectRight.fill()
-
-
 				else:
 					## using a bezier path instead of an NSRect for transforming ability
 					pathRect = NSBezierPath.bezierPath()
@@ -234,103 +141,49 @@ class LabelColor ( NSObject, GlyphsReporterProtocol ):
 					pathRect.lineToPoint_( (rectangle[2] + self.angle(rectangle[1], thisXHeight, thisAngle), rectangle[1]) )
 					pathRect.closePath()
 
-					pathRect.fill()					
-
+					pathRect.fill()
 			except:
-				# print traceback.format_exc()
-				self.logToConsole(traceback.format_exc())
-
-
-		# except Exception as e:
-		# 	self.logToConsole( "LabelColor: %s" % str(e) )
+				print traceback.format_exc()
 		except:
-			self.logToConsole( "LabelColor: %s" % str(traceback.format_exc()) )
+			print traceback.format_exc()
 			
-	def drawBackgroundForLayer_( self, Layer ):
+	def background( self, layer ):
 		"""
 		Whatever you draw here will be displayed BEHIND the paths.
 		"""
 		try:
-			# pass
-			self.LabelColor( Layer )
+			self.LabelColor( layer )
 			# self.BlockOutGlyph( Layer )
-		except Exception as e:
-			self.logToConsole( "drawBackgroundForLayer_: %s" % str(e) )
+		except:
+			print traceback.format_exc()
 	
-	def drawBackgroundForInactiveLayer_( self, Layer ):
+	def inactiveLayers(self, layer):
 		"""
 		Whatever you draw here will be displayed behind the paths, but for inactive masters.
 		"""
 		try:
-			pass
-			self.LabelColor( Layer )
+			self.LabelColor( layer )
 			# self.BlockOutGlyph( Layer )
-		except Exception as e:
-			self.logToConsole( "drawBackgroundForInactiveLayer_: %s" % str(e) )
-	
-	def drawTextAtPoint( self, text, textPosition, fontSize=9.0, fontColor=NSColor.brownColor() ):
-		"""
-		Use self.drawTextAtPoint( "blabla", myNSPoint ) to display left-aligned text at myNSPoint.
-		"""
-		try:
-			glyphEditView = self.controller.graphicView()
-			currentZoom = self.getScale()
-			fontAttributes = { 
-				NSFontAttributeName: NSFont.labelFontOfSize_( fontSize/currentZoom ),
-				NSForegroundColorAttributeName: fontColor }
-			displayText = NSAttributedString.alloc().initWithString_attributes_( text, fontAttributes )
-			textAlignment = 3 # top left: 6, top center: 7, top right: 8, center left: 3, center center: 4, center right: 5, bottom left: 0, bottom center: 1, bottom right: 2
-			glyphEditView.drawText_atPoint_alignment_( displayText, textPosition, textAlignment )
-		except Exception as e:
-			self.logToConsole( "drawTextAtPoint: %s" % str(e) )
+		except:
+			print traceback.format_exc()
 	
 	def needsExtraMainOutlineDrawingForInactiveLayer_( self, Layer ):
 		return True
-
 	
-	def getHandleSize( self ):
-		"""
-		Returns the current handle size as set in user preferences.
-		Use: self.getHandleSize() / self.getScale()
-		to determine the right size for drawing on the canvas.
-		"""
-		try:
-			Selected = NSUserDefaults.standardUserDefaults().integerForKey_( "GSHandleSize" )
-			if Selected == 0:
-				return 5.0
-			elif Selected == 2:
-				return 10.0
-			else:
-				return 7.0 # Regular
-		except Exception as e:
-			self.logToConsole( "getHandleSize: HandleSize defaulting to 7.0. %s" % str(e) )
-			return 7.0
-
-	def getScale( self ):
-		"""
-		self.getScale() returns the current scale factor of the Edit View UI.
-		Divide any scalable size by this value in order to keep the same apparent pixel size.
-		"""
-		try:
-			return self.controller.graphicView().scale()
-		except:
-			self.logToConsole( "Scale defaulting to 1.0" )
-			return 1.0
+	# def setController_( self, Controller ):
+	# 	"""
+	# 	Use self.controller as object for the current view controller.
+	# 	"""
+	# 	try:
+	# 		self.controller = Controller
+	# 	except Exception as e:
+	# 		self.logToConsole( "Could not set controller" )
 	
-	def setController_( self, Controller ):
-		"""
-		Use self.controller as object for the current view controller.
-		"""
-		try:
-			self.controller = Controller
-		except Exception as e:
-			self.logToConsole( "Could not set controller" )
-	
-	def logToConsole( self, message ):
-		"""
-		The variable 'message' will be passed to Console.app.
-		Use self.logToConsole( "bla bla" ) for debugging.
-		"""
-		myLog = "Show %s plugin:\n%s" % ( self.title(), message )
-		# print myLog
-		NSLog( myLog )
+	# def logToConsole( self, message ):
+	# 	"""
+	# 	The variable 'message' will be passed to Console.app.
+	# 	Use self.logToConsole( "bla bla" ) for debugging.
+	# 	"""
+	# 	myLog = "Show %s plugin:\n%s" % ( self.title(), message )
+	# 	# print myLog
+	# 	NSLog( myLog )
